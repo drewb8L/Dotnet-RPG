8e5cf9d0db508c21f3b4a449ef1b8e115813bd68
drewb8L <104442990+drewb8L@users.noreply.github.com>
2023-04-17 23:52:33 -0500

    sec-3:32 First migration

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+namespace Dotnet_RPG.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Character> Characters => Set<Character>();
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 global using Dotnet_RPG.Models;
+global using Microsoft.EntityFrameworkCore;
+global using Dotnet_RPG.Data;
 using Dotnet_RPG.Services.CharacterService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
